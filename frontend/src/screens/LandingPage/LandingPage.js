759bcf05796ac99d7c73541dd78450a6773d8d9b
David Musau <davie23musa@gmail.com>
2024-01-23 13:53:13 +0300

    Landing page finished

--- frontend/src/screens/LandingPage/LandingPage.js
+++ frontend/src/screens/LandingPage/LandingPage.js
@@ -7,57 +7,61 @@ import TeamMeetingBeforeKickOff from "./images/TeamMeetingBeforeKickOff.jpg"
 
 const LandingPage = () => {
   return (
-    <div>
-      <>
-            <Carousel data-bs-theme="dark">
-                <Carousel.Item interval={1000}>
-                    <img
-                        className="d-block w-100"
-                        src={ReferreesTraining}
-                        alt="First slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>This is the first slide!</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item interval={500}>
-                    <img
-                        className="d-block w-100"
-                        src={BrewedTalent}
-                        alt="Second slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={NoshInAction}
-                        alt="Third slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={TeamMeetingBeforeKickOff}
-                        alt="Third slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>Fourth slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
-        </>
-    </div>
-  )
+    <section>
+        <div class="relative pt-16 pb-32 flex content-center items-center justify-center psting">
+      <div
+        class="bg-img absolute top-0 w-full h-full bg-center bg-cover"
+      >
+        <span
+          id="blackOverlay"
+          class="w-full h-full absolute opacity-75 bg-black"
+        ></span>
+      </div>
+        <div class="container relative mx-auto">
+            <div class="flex flex-wrap justify">
+                <div class="grow lg:w-6/12 px-4 ml-auto mr-auto">
+                <Carousel>
+                    <Carousel.Item interval={1000}>
+                        <img src={BrewedTalent} alt="Brewed Talent Image"/>
+                        <Carousel.Caption>
+                        <h3>Masii FC vs Kathekani FC</h3>
+                        <p>A friendly away match with Kathekani FC emerged winners.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item interval={500}>
+                    <img src={ReferreesTraining} alt="Referrees Training Image"/>
+                        <Carousel.Caption>
+                        <h3>Referrees Training Award</h3>
+                        <p>Four of our own players gets certified to officiate FKF matched after a rigorous training held at Tawa social hall.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                    <img src={NoshInAction} alt="Nosh Inaction Image" />
+                        <Carousel.Caption>
+                        <h3>SCOTTLANDERS FC 5 : 3 KATHEKANI FC</h3>
+                        <p>
+                            Kathekani FC Suffered an away defeat against Scotlanders FC on a Sunday.
+                        </p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    </Carousel>
+                </div>
+                <div class="justify-end lg:w-6/12 px-4 ml-auto mr-auto text-center">
+                    <div class="pr-12">
+                        <h1 class="text-white font-semibold text-2xl">
+                            KATHEKANI FOOTBALL CLUB
+                        </h1>
+                        <p class="mt-4 text-lg text-gray-300">
+                        Founded in 2008, our achievements include winning the Crll. Kalia Cup in 2010, the 2013 Dr. Musembi Cup (Songeni), and consecutive victories in the Ligi Mashinani in 2016, 2017, 2018, 2019, and 2020. Additionally, we claimed victory in the Muting'a Cup in 2018, the Katumo Cup in 2020, and the Darius Cup in 2022.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    </section>
+       
+  );
 }
 
 export default LandingPage
